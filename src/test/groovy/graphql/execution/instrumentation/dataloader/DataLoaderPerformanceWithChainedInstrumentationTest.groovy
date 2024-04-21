fd2c5ca9e83eb7209f3f0aca5e9dbc9faf150e1d
Felipe Reis <freis@atlassian.com>
2024-04-22 09:41:39 +1000

    WIP: Preparing code for adding support for defer + dataloaders

--- src/test/groovy/graphql/execution/instrumentation/dataloader/DataLoaderPerformanceWithChainedInstrumentationTest.groovy
+++ src/test/groovy/graphql/execution/instrumentation/dataloader/DataLoaderPerformanceWithChainedInstrumentationTest.groovy
@@ -141,7 +141,6 @@ class DataLoaderPerformanceWithChainedInstrumentationTest extends Specification 
         exception.message == "Data Loaders cannot be used to resolve deferred fields"
     }
 
-    @Ignore("Resolution of deferred fields via Data loaders is not yet supported")
     def "chainedInstrumentation: data loader will work with deferred queries"() {
 
         when:
@@ -152,7 +151,7 @@ class DataLoaderPerformanceWithChainedInstrumentationTest extends Specification 
                 .graphQLContext([(ENABLE_INCREMENTAL_SUPPORT): true])
                 .build()
 
-        IncrementalExecutionResult result = graphQL.execute(executionInput)
+        IncrementalExecutionResult result = graphQL.execute(executionInput) as IncrementalExecutionResult
 
         then:
         result.toSpecification() == expectedInitialDeferredData
